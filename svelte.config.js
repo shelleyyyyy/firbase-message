ce272b3759c961439c5330bb4962458f9b39bff5
shelleyyyyy <shelleywr23@mail.vmi.edu>
2022-12-27 11:26:22 -0500

    more

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,15 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	},
-	preprocess: vitePreprocess()
-
+	preprocess: [
+		vitePreprocess(),
+	]
 };
 
 export default config;
